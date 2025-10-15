6325b01d6f1c731500af49051121b4d704763eb6
Yassine <yassine.zriba@fms-ea.com>
2025-10-15 14:41:02 +0200

    dice roll and speed evolution function

--- harness_racing_simulator/harness_racing_simulator.py
+++ harness_racing_simulator/harness_racing_simulator.py
@@ -3,15 +3,35 @@
 
 import random
 
-def creer_chevaux(nb) :
+def creat_horse(nb) :
 
-	chevaux = []
+	horse = []
 	for i in range(nb) :
-		chevaux.append({
+		horse.append({
 			"nom": f"Chevaux {i+1}",
 			"vitesse": 0,
 			"distance":0,
 			"disqualifie": False
 		})
-	return chevaux
+	return horse
+
+def dice_roll():
+	"""Renvoie un nombre de dé aleatoire entre 1 et 6"""
+	return random.randint(1,6)
+
+def speed_evolution(speed, roll) :
+	"""Retourne la nouvelle vitesse selon le tableau donner"""
+	table = {
+		0: [0, +1, +1, +1, +2, +2],
+		1: [0, 0, +1, +1, +1, +2],
+		2: [0, 0, +1, +1, +1, +2],
+		3: [-1, 0, 0, +1, +1, +1],
+		4: [-1, 0, 0, 0, +1, +1],
+		5: [-2, -1, 0, 0, 0, +1],
+		6: [-2, -1, 0, 0, 0, "DQ"],
+	}
+	variation = table[speed][roll-1]
+	if variation == "DQ" :
+		return None 	# disqualification
+	return max(0, speed + variation)
 
